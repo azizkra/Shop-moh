20ba5653f6f26c049ec1b2cf63c53aa95b5b480f
azizkrayyem <95894853+azizkra@users.noreply.github.com>
2023-07-27 23:05:50 +0300

    add new file01111111

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,9 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hf2yo5^itx^7x*%@hjm9(fbph&_)i%8rb)$qu0+su+v_p$ex#j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS =  ['web-production-63c2.up.railway.app', '127.0.0.1:8000']
+ALLOWED_HOSTS =  ['*']
 
 
 # Application definition
